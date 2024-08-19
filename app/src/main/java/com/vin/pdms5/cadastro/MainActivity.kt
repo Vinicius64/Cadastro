9955b02d9437d9bce021624a259d35d8dd7a1a4e
Vinicius64 <oliveira.vinicius64@gmail.com>
2024-08-18 22:30:48 -0300

    feat(cadastro): estados inseridos para seleção no spinner

--- app/src/main/java/com/vin/pdms5/cadastro/MainActivity.kt
+++ app/src/main/java/com/vin/pdms5/cadastro/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.vin.pdms5.cadastro
 
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import androidx.appcompat.app.AppCompatActivity
 import com.vin.pdms5.cadastro.databinding.ActivityMainBinding
 
@@ -11,5 +12,11 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         amb = ActivityMainBinding.inflate(layoutInflater)
         setContentView(amb.root)
+
+        val estados = resources.getStringArray(R.array.estados_brasil)
+        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, estados)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        amb.sUF.adapter = adapter
+
     }
 }
