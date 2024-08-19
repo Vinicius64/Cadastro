68077110f25e539f2b45775dee3f5542249ad260
Vinicius64 <oliveira.vinicius64@gmail.com>
2024-08-18 22:13:37 -0300

    feat(cadastro): iniciado a base do onCreate

--- app/src/main/java/com/vin/pdms5/cadastro/MainActivity.kt
+++ app/src/main/java/com/vin/pdms5/cadastro/MainActivity.kt
@@ -1,8 +1,15 @@
 package com.vin.pdms5.cadastro
 
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.vin.pdms5.cadastro.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var amb: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?){
+        super.onCreate(savedInstanceState)
+        amb = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(amb.root)
+    }
 }
