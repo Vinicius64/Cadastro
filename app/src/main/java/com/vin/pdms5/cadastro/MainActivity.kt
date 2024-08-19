fbecd16c113c6193761ee8ee04607b33d8015a5e
Vinicius64 <oliveira.vinicius64@gmail.com>
2024-08-18 22:11:18 -0300

    feat(cadastro): adiciona o ActivityMainBinding

--- app/src/main/java/com/vin/pdms5/cadastro/MainActivity.kt
+++ app/src/main/java/com/vin/pdms5/cadastro/MainActivity.kt
@@ -1,7 +1,8 @@
 package com.vin.pdms5.cadastro
 
 import androidx.appcompat.app.AppCompatActivity
+import com.vin.pdms5.cadastro.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-
+    private lateinit var amb: ActivityMainBinding
 }
