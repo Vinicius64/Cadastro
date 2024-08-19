c30d5b2c87a601ed6cfc2cd1574fa0eb88e6ad3b
Vinicius64 <oliveira.vinicius64@gmail.com>
2024-08-18 22:53:12 -0300

    feat(cadastro): finalizada função onCreate e corrigida atribuição de tipagem

--- app/src/main/java/com/vin/pdms5/cadastro/Formulario.kt
+++ app/src/main/java/com/vin/pdms5/cadastro/Formulario.kt
@@ -1,15 +1,15 @@
 package com.vin.pdms5.cadastro
 
 data class Formulario (
-    val fullName: string,
-    val phone: string,
-    val mail: string,
-    val joinEmailList: boolean,
-    val city: string,
-    val uf: string,
-    val gender: string
+    val fullName: String,
+    val phone: String,
+    val mail: String,
+    val joinEmailList: Boolean,
+    val city: String,
+    val uf: String,
+    val gender: String
 ){
-    override fun toString(): string {
+    override fun toString(): String {
         return "Nome Completo: $fullName\nTelefone: $phone\nEmail: $mail\nIngressar na lista de emails: $joinEmailList\nCidade: $city\nUF: $uf\nSexo: $gender"
     }
 }
--- app/src/main/java/com/vin/pdms5/cadastro/MainActivity.kt
+++ app/src/main/java/com/vin/pdms5/cadastro/MainActivity.kt
@@ -2,13 +2,15 @@ package com.vin.pdms5.cadastro
 
 import android.os.Bundle
 import android.widget.ArrayAdapter
+import android.widget.RadioButton
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.vin.pdms5.cadastro.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var amb: ActivityMainBinding
 
-    override fun onCreate(savedInstanceState: Bundle?){
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         amb = ActivityMainBinding.inflate(layoutInflater)
         setContentView(amb.root)
@@ -18,5 +20,42 @@ class MainActivity : AppCompatActivity() {
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
         amb.sUF.adapter = adapter
 
+        fun validateFields(): Boolean {
+            return amb.etFullName.text.isNotEmpty() &&
+                    amb.etTelephone.text.isNotEmpty() &&
+                    amb.etMail.text.isNotEmpty() &&
+                    amb.etCity.text.isNotEmpty() &&
+                    amb.rgGender.checkedRadioButtonId != -1
+        }
+
+        amb.btnSave.setOnClickListener {
+            if (validateFields()) {
+                val selectedGenderId = amb.rgGender.checkedRadioButtonId
+                val selectedGender = findViewById<RadioButton>(selectedGenderId).text.toString()
+
+                val form = Formulario(
+                    fullName = amb.etFullName.text.toString(),
+                    phone = amb.etTelephone.text.toString(),
+                    mail = amb.etMail.text.toString(),
+                    joinEmailList = amb.cbJoinMailList.isChecked,
+                    city = amb.etCity.text.toString(),
+                    uf = amb.sUF.selectedItem.toString(),
+                    gender = selectedGender
+                )
+                Toast.makeText(this, form.toString(), Toast.LENGTH_LONG).show()
+            } else {
+                Toast.makeText(this, "Todos os campos devem ser preenchidos", Toast.LENGTH_LONG)
+                    .show()
+            }
+        }
+        amb.btnClear.setOnClickListener {
+            amb.etFullName.text.clear()
+            amb.etTelephone.text.clear()
+            amb.etMail.text.clear()
+            amb.cbJoinMailList.isChecked = false
+            amb.etCity.text.clear()
+            amb.sUF.setSelection(0)
+            amb.rgGender.clearCheck()
+        }
     }
 }
